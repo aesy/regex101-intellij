f907fdc90b707bbc078622cf8c7df1f1a56a4dce
Isak W <isak.wertwein@gmail.com>
2020-06-07 01:00:00 +0200

    Support all available regex types

--- src/main/kotlin/io/aesy/regex101/OpenRegex101Intention.kt
+++ src/main/kotlin/io/aesy/regex101/OpenRegex101Intention.kt
@@ -2,6 +2,7 @@ package io.aesy.regex101
 
 import com.intellij.codeInsight.intention.impl.QuickEditAction
 import com.intellij.ide.BrowserUtil
+import com.intellij.lang.Language
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Iconable
@@ -9,6 +10,7 @@ import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.intellij.lang.regexp.RegExpLanguage
+import java.net.URLEncoder
 import javax.swing.Icon
 
 class OpenRegex101Intention : QuickEditAction(), Iconable {
@@ -33,13 +35,28 @@ class OpenRegex101Intention : QuickEditAction(), Iconable {
             return
         }
 
-        val regex = StringUtil.unescapeBackSlashes(element.text)
-        val url = "$domain/?regex=$regex&flavor=pcre"
+        val flavor = element.language.toFlavor()
+        val regex = urlEncode(getText(element))
+        val url = "$domain/?regex=$regex&flavor=$flavor"
 
         BrowserUtil.browse(url)
     }
 
     private fun getElement(file: PsiFile, editor: Editor): PsiElement {
         return getRangePair(file, editor)?.first ?: file
+    }
+
+    private fun getText(element: PsiElement): String = when (element.language.id) {
+        "RegExp", "XsdRegExp" -> StringUtil.unescapeBackSlashes(element.text)
+        else -> element.text
+    }
+
+    private fun urlEncode(text: String): String = URLEncoder.encode(text, Charsets.UTF_8)
+
+    private fun Language.toFlavor(): String = when (this.id) {
+        "JSRegexp", "JSUnicodeRegexp" -> "javascript"
+        "PythonRegExp", "PythonVerboseRegExp" -> "python"
+        "GoRegExp" -> "golang"
+        else -> "pcre"
     }
 }
