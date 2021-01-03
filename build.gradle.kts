be0b89d3039e4edcbe83983d1f3365d15bdd3ba9
Isak W <isak.wertwein@gmail.com>
2021-01-03 14:04:35 +0100

    Add custom junit5 extension for intellij platform tests
    This removes the need for junit vintage engine, and adds better cleanup by recreating the project object between tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
     testImplementation("io.strikt:strikt-core:0.26.1")
 }
 
--- src/test/kotlin/io/aesy/regex101/OpenRegex101IntentionTest.kt
+++ src/test/kotlin/io/aesy/regex101/OpenRegex101IntentionTest.kt
@@ -3,22 +3,28 @@ package io.aesy.regex101
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.injection.Injectable
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import org.intellij.lang.regexp.RegExpFileType
 import org.intellij.lang.regexp.RegExpLanguage
 import org.intellij.plugins.intelliLang.inject.InjectLanguageAction
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import strikt.api.expectThat
 import strikt.assertions.contains
 import strikt.assertions.containsExactly
 import strikt.assertions.doesNotContain
 import strikt.assertions.map
 
-class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
+@ExtendWith(IntelliJExtension::class)
+class OpenRegex101IntentionTest {
     companion object {
         const val actionName: String = "Open RegExp on regex101.com"
     }
+
+    @IntelliJ
+    private lateinit var fixture: CodeInsightTestFixture
 
     @AfterEach
     fun teardown() {
@@ -28,12 +34,12 @@ class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
     @Test
     @DisplayName("It should not display a OpenRegex101 Intention in JavaScript elements")
     fun testIntentionDoesNotExistsInJs() {
-        val file = myFixture.configureByFile("regex.js")
+        val file = fixture.configureByFile("regex.js")
         val offset = StringUtil.lineColToOffset(file.text, 0, 1)
 
-        WriteAction.runAndWait<Throwable> { myFixture.editor.caretModel.moveToOffset(offset) }
+        WriteAction.runAndWait<Throwable> { fixture.editor.caretModel.moveToOffset(offset) }
 
-        expectThat(myFixture.availableIntentions)
+        expectThat(fixture.availableIntentions)
             .map { it.text }
             .doesNotContain(actionName)
     }
@@ -41,12 +47,12 @@ class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
     @Test
     @DisplayName("It should display a OpenRegex101 Intention in regular expression elements")
     fun testIntentionExistsInRegex() {
-        val file = myFixture.configureByFile("regex.js")
+        val file = fixture.configureByFile("regex.js")
         val offset = StringUtil.lineColToOffset(file.text, 0, 17)
 
-        WriteAction.runAndWait<Throwable> { myFixture.editor.caretModel.moveToOffset(offset) }
+        WriteAction.runAndWait<Throwable> { fixture.editor.caretModel.moveToOffset(offset) }
 
-        expectThat(myFixture.availableIntentions)
+        expectThat(fixture.availableIntentions)
             .map { it.text }
             .contains(actionName)
     }
@@ -54,14 +60,14 @@ class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
     @Test
     @DisplayName("It should be able to launch OpenRegex101 action from JavaScript regular expression")
     fun testLaunchActionJavaScript() {
-        val file = myFixture.configureByFile("regex.js")
+        val file = fixture.configureByFile("regex.js")
         val offset = StringUtil.lineColToOffset(file.text, 0, 17)
 
-        WriteAction.runAndWait<Throwable> { myFixture.editor.caretModel.moveToOffset(offset) }
+        WriteAction.runAndWait<Throwable> { fixture.editor.caretModel.moveToOffset(offset) }
 
-        val intention = myFixture.findSingleIntention(actionName)
+        val intention = fixture.findSingleIntention(actionName)
 
-        myFixture.launchAction(intention)
+        fixture.launchAction(intention)
 
         expectThat(TestUtil.openedUrls)
             .containsExactly("https://regex101.com/?regex=.%2B&flavor=javascript")
@@ -70,18 +76,18 @@ class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
     @Test
     @DisplayName("It should be able to launch OpenRegex101 action from regular expression injected in Java file")
     fun testLaunchActionJava() {
-        val file = myFixture.configureByFile("pattern.java")
+        val file = fixture.configureByFile("pattern.java")
         val offset = StringUtil.lineColToOffset(file.text, 4, 45)
         val injectable = Injectable.fromLanguage(RegExpLanguage.INSTANCE)
 
         WriteAction.runAndWait<Throwable> {
-            myFixture.editor.caretModel.moveToOffset(offset)
-            InjectLanguageAction.invokeImpl(project, myFixture.editor, myFixture.file, injectable)
+            fixture.editor.caretModel.moveToOffset(offset)
+            InjectLanguageAction.invokeImpl(project, fixture.editor, fixture.file, injectable)
         }
 
-        val intention = myFixture.findSingleIntention(actionName)
+        val intention = fixture.findSingleIntention(actionName)
 
-        myFixture.launchAction(intention)
+        fixture.launchAction(intention)
 
         expectThat(TestUtil.openedUrls)
             .containsExactly("https://regex101.com/?regex=.%2B&flavor=pcre")
@@ -90,14 +96,14 @@ class OpenRegex101IntentionTest : JUnit5CodeInsightTest() {
     @Test
     @DisplayName("It should be able to launch OpenRegex101 action from regular expression file")
     fun testLaunchActionRegex() {
-        val file = myFixture.configureByText(RegExpFileType.INSTANCE, ".+")
+        val file = fixture.configureByText(RegExpFileType.INSTANCE, ".+")
         val offset = StringUtil.lineColToOffset(file.text, 0, 2)
 
-        WriteAction.runAndWait<Throwable> { myFixture.editor.caretModel.moveToOffset(offset) }
+        WriteAction.runAndWait<Throwable> { fixture.editor.caretModel.moveToOffset(offset) }
 
-        val intention = myFixture.findSingleIntention(actionName)
+        val intention = fixture.findSingleIntention(actionName)
 
-        myFixture.launchAction(intention)
+        fixture.launchAction(intention)
 
         expectThat(TestUtil.openedUrls)
             .containsExactly("https://regex101.com/?regex=.%2B&flavor=pcre")
